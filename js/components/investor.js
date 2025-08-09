fbf35032993e06159a52c79d1949cbd70416a72c
Realmz1 <123913143+Realmz1@users.noreply.github.com>
2025-08-09 16:14:23 -0700

    Update investor.js

--- js/components/investor.js
+++ js/components/investor.js
@@ -1,9 +1,8 @@
-import { qs } from '../utils.mjs';
-import { setInvestor } from '../state.mjs';
+import { qs } from '../utils.js';
+import { setInvestor } from '../state.js';
 
 export function renderInvestorToggle(el, state, onToggle) {
   const id = 'investorToggle';
-  el.innerHTML = `<label><input type="checkbox" id="${id}" /> Investor Mode</label>`;
   el.innerHTML = `<label class="toggle"><input type="checkbox" id="${id}" /> Investor Mode</label>`;
   const box = qs(`#${id}`, el);
   box.checked = state.investor || false;
