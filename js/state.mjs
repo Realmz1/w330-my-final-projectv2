df5d98725c91c0fd19d4f9fb331f104313320afd
Realmz1 <123913143+Realmz1@users.noreply.github.com>
2025-08-13 17:29:21 -0700

    Update state.mjs

--- js/state.mjs
+++ js/state.mjs
@@ -1,8 +1,22 @@
 import { storage } from './utils.mjs';
 
+const savedQuiz = storage.get('quiz');
+const defaultQuiz =
+  savedQuiz || {
+    income: 85000,
+    rent: 1800,
+    savings: 20000,
+    credit: '680-740',
+    timeframe: '3-6',
+    city: 'Denver, CO',
+    rentIncome: 2400,
+  };
+
+if (!savedQuiz) storage.set('quiz', defaultQuiz);
+
 const defaultState = {
   scenarios: storage.get('scenarios') || [],
-  quiz: {},
+  quiz: defaultQuiz,
   investor: storage.get('investor') || false,
 };
 
@@ -11,6 +25,11 @@ export const state = defaultState;
 export const saveScenario = scenario => {
   state.scenarios.push(scenario);
   storage.set('scenarios', state.scenarios);
+};
+
+export const saveQuiz = quiz => {
+  state.quiz = quiz;
+  storage.set('quiz', quiz);
 };
 
 export const setInvestor = val => {
